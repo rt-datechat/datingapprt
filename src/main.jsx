e705e05b3ee3f7fa27f8ab71b19ae6a5672401a2
ridhakurfi <ridhakurfi@gmail.com>
2024-02-21 17:21:14 +0700

    feat login

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-import { ChatContext, ChatContextProvider } from "./context/ChatContext.jsx";
+import { ChatContextProvider } from "./context/ChatContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
